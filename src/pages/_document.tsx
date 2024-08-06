fe0eb2a8f2cb074c5289c7da3c80a9383cc5ee10
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-06 17:43:21 -0400

    refactor(document): remove language comment

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="es-419"> # Spanish for Latin America
+    <Html lang="es-419">
       <Head />
       <body>
         <Main />
