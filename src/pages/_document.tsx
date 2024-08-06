3991dc6aab472eda008fc177f03005eb648753f5
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-06 17:28:49 -0400

    feat(document): change language to spanish (latin america)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es-419"> # Spanish for Latin America
       <Head />
       <body>
         <Main />
