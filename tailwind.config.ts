7daa75b91beb8e6b5c6feb21127444edacec3f7d
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-06 17:14:07 -0400

    style: update tailwind config and global styles with custom color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "rgb(var(--color-primary) / <alpha-value>)",
+                secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+                "dark-green": "rgb(var(--color-dark-green) / <alpha-value>)",
+                "light-green": "rgb(var(--color-light-green) / <alpha-value>)",
+                white: "rgb(var(--color-white) / <alpha-value>)",
+                gray: "rgb(var(--color-gray) / <alpha-value>)",
+            },
+        },
+    },
+    plugins: [],
 };
 export default config;
