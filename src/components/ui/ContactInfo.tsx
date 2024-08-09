48e86d5ae84a6c7721e088e539f64ff2a447d0c4
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-09 17:31:28 -0400

    chore(permissions): update file permissions to ensure user access

--- src/components/ui/ContactInfo.tsx
+++ src/components/ui/ContactInfo.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import Logo from './Logo';
+import Logo from "./Logo";
 
 const ContactInfo = () => (
     <div className="flex flex-col items-start gap-4">
-        <Link href="#" className="flex items-center gap-2" prefetch={false}>
-            <Logo />
-            <span className="sr-only">Verdudelivery</span>
-        </Link>
+        <Logo />
+        <span className="sr-only">Verdudelivery</span>
         <div className="grid gap-1 text-muted-foreground">
             <div>info@verdudelivery.com</div>
             <div>+56 9 9078 5716</div>
