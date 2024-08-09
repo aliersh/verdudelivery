fff1d8004490b4d163732c175bc785712360c658
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-09 16:12:10 -0400

    feat(ui): add padding and shadow to CardContent component
    apply 'p-6 shadow-md' classes to Card component within CardContent for improved styling

--- src/components/ui/CardContent.tsx
+++ src/components/ui/CardContent.tsx
@@ -10,7 +10,7 @@ const CardContent = ({
     title: string;
     children: React.ReactNode;
 }) => (
-    <Card>
+    <Card className='p-6 shadow-md'>
         <div className="flex flex-col items-center gap-4 p-6">
             <CardIcon Icon={Icon} />
             <h3 className="text-xl font-bold">{title}</h3>
