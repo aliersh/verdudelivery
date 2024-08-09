d79fc8e1da18cce1a662af07e92817d9e5380237
aliersh <ariel.diaz.borquez@icloud.com>
2024-08-09 16:15:48 -0400

    feat(layouts): add Footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Navbar />
             <Component {...pageProps} />
+            <Footer />
         </>
     );
 }
