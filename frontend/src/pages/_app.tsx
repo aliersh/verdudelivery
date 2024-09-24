3dac6bb60c3bd8ffa3cadf2ee5099cde84799d77
aliersh <ariel.diaz.borquez@icloud.com>
2024-09-24 10:48:01 -0400

    build(frontend): integrate MedusaProvider in _app.tsx
    configured MedusaProvider with base URL and QueryClient in _app.tsx to connect frontend with Medusa backend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,24 @@
-import Navbar from "@/components/layouts/Navbar";
-import Footer from "@/components/layouts/Footer";
 import "@/styles/globals.css";
+
+import { MedusaProvider } from "medusa-react";
+
+import Footer from "@/components/layouts/Footer";
+import Navbar from "@/components/layouts/Navbar";
+import { QueryClient } from "@tanstack/react-query";
+
 import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <MedusaProvider
+            baseUrl="http://localhost:9000"
+            queryClientProviderProps={{ client: queryClient }}
+        >
             <Navbar />
             <Component {...pageProps} />
             <Footer />
-        </>
+        </MedusaProvider>
     );
 }
