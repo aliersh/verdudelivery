e3a72f45f1fb0f50f8536f25bad824e51eeee5fa
aliersh <ariel.diaz.borquez@icloud.com>
2024-09-24 17:49:56 -0400

    chore(config): update next.config.js to accept images from localhost and Medusa public images
    added localhost and medusa-public-images.s3.eu-west-1.amazonaws.com to images domains

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "localhost",
+            "medusa-public-images.s3.eu-west-1.amazonaws.com",
+        ],
+    },
 };
 
 export default nextConfig;
