e4b578be7f3f9803b186c448b54478f3726fe02b
aliersh <ariel.diaz.borquez@icloud.com>
2024-09-26 17:35:21 -0400

    style(footer): remove max width class from container

--- frontend/src/components/layouts/Footer.tsx
+++ frontend/src/components/layouts/Footer.tsx
@@ -7,7 +7,7 @@ import SocialLinks from '../ui/SocialLinks';
 const Footer = () => {
     return (
         <footer className="w-full py-8 bg-muted md:py-12">
-            <div className="container grid grid-cols-1 gap-8 max-w-7xl sm:grid-cols-2 md:grid-cols-3">
+            <div className="container grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
                 <ContactInfo />
                 <div className="flex items-center justify-center">
                     <SocialLinks
