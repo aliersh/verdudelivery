0d7915e43b3ee1cbdb149d21c2332d6b8678c387
aliersh <ariel.diaz.borquez@icloud.com>
2024-09-27 19:24:07 -0400

    fix(next.js): update Supabase image path configuration in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
             "localhost",
             "medusa-public-images.s3.eu-west-1.amazonaws.com",
         ],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "vgefhivfjoaxzlsbtkbp.supabase.co",
+                pathname: "/storage/v1/object/public/**", // Allow access to any objects in your S3 bucket
+            },
+        ],
     },
 };
 
