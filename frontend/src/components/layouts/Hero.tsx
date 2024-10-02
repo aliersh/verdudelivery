e7f7c08839aef69d60a3007be2c713a5d2139c70
aliersh <ariel.diaz.borquez@icloud.com>
2024-10-02 15:13:11 -0400

    chore(assets): move background video from /public to /public/videos

--- frontend/src/components/layouts/Hero.tsx
+++ frontend/src/components/layouts/Hero.tsx
@@ -1,11 +1,11 @@
-import HeroContent from '../ui/HeroContent';
-import Overlay from '../ui/Overlay';
-import VideoBackground from '../ui/VideoBackground';
+import HeroContent from "../ui/HeroContent";
+import Overlay from "../ui/Overlay";
+import VideoBackground from "../ui/VideoBackground";
 
 const Hero = () => {
     return (
         <main className="relative flex h-[50vh] w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat">
-            <VideoBackground src="130226-746395325_small.mp4" />
+            <VideoBackground src="/videos/130226-746395325_small.mp4" />
             <Overlay />
             <HeroContent />
         </main>
