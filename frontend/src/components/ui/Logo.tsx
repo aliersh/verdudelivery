c08083d9763eb6e224f4335dbd9ffccdae8910c2
aliersh <ariel.diaz.borquez@icloud.com>
2024-10-03 18:08:15 -0400

    fix(logo): update href from '#' to '/'

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Logo = () => (
-    <Link href="#" className="text-2xl font-bold text-primary" prefetch={false}>
+    <Link href="/" className="text-2xl font-bold text-primary" prefetch={false}>
         verdu<span className="text-destructive">delivery</span>
     </Link>
 );
