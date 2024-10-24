99356e3d232a5f0311ddb4ff791addd364cfe71b
aliersh <ariel.diaz.borquez@icloud.com>
2024-10-24 18:15:32 -0400

    refactor(navbar): move Navbar component to layout folder

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 import { ReactNode } from 'react';
 
-import Navbar from '@/components/ui/Navbar';
+import Navbar from '@/components/layout/Navbar';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
