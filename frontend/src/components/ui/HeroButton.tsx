feb329fecb8a972d732f38a93902c9eaac5978e6
aliersh <ariel.diaz.borquez@icloud.com>
2024-10-29 19:18:03 -0400

    feat(HeroButton): add Link component to HeroButtons for navigation

--- frontend/src/components/ui/HeroButton.tsx
+++ frontend/src/components/ui/HeroButton.tsx
@@ -2,13 +2,14 @@
 
 import { FC } from "react";
 import { Button } from '@/components/ui/button';
+import Link from "next/link";
 
 const HeroButtons: FC = () => {
     return (
         <div className="flex justify-center gap-3 mt-8">
             <Button size={"lg"}>Registrate</Button>
             <Button size={"lg"} variant={"accent"}>
-                Ver productos
+                <Link href="/productos">Ver productos</Link>
             </Button>
         </div>
     );
